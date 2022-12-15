cbfd332ff09e9d2bdb439bb364303f0a0635427b
Mulugeta Mebt <mulugetamebt@gmail.com>
2022-12-15 06:34:35 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,20 +11,24 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b)
+{
+if (a > c)
 {
 largest = a;
 }
-else if (b > c && b > a)
+else
+{
+largest c;
+}
+}
+else if (b > c)
 {
 largest = b;
-else if (c > b)
+}
+else
 {
 largest = c;
-}
-else 
-{
-largest = b;
 }
 
 return (largest);
